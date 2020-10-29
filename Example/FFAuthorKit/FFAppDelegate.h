79ec08c7c4d7e28c8cecec056c65980208f570bc
chenhuijin <chenhj@actumtech.com>
2020-10-29 13:42:57 +0800

    Initial commit

---
+++ Example/FFAuthorKit/FFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FFAppDelegate.h
+//  FFAuthorKit
+//
+//  Created by Chj1995 on 10/29/2020.
+//  Copyright (c) 2020 Chj1995. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
