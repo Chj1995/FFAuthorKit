c43158b6e394ffd071bea2e6205db3a0b9d79df3
chenhuijin <chenhj@actumtech.com>
2020-10-29 15:12:53 +0800

    修改层级1.0.8

--- Example/Pods/Target Support Files/FFAuthorKit/FFAuthorKit-umbrella.h
+++ Example/Pods/Target Support Files/FFAuthorKit/FFAuthorKit-umbrella.h
@@ -11,19 +11,17 @@
 #endif
 
 #import "FFAuthorDetailController.h"
+#import "FFAuthorKit.h"
 #import "AuthorAPIRequest.h"
 #import "FFAuthorCell.h"
 #import "FFAuthorController.h"
-#import "FFAuthorKit.h"
 #import "FFAuthorListReformer.h"
 #import "Target_Author.h"
 #import "FFAuthorDetailController.h"
 #import "AuthorAPIRequest.h"
 #import "FFAuthorCell.h"
 #import "FFAuthorController.h"
-#import "FFAuthorKit.h"
 #import "FFAuthorListReformer.h"
-#import "Target_Author.h"
 
 FOUNDATION_EXPORT double FFAuthorKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char FFAuthorKitVersionString[];
--- FFAuthorKit.podspec
+++ FFAuthorKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'FFAuthorKit'
-  s.version          = '1.0.7'
+  s.version          = '1.0.8'
   s.summary          = 'A short description of FFAuthorKit.'
 
 # This description is used to generate tags and improve search results.
@@ -35,6 +35,7 @@ TODO: Add long description of the pod here.
   s.subspec 'Detail' do |ss|
       ss.source_files = 'FFAuthorKit/Classes/Detail/*.{h,m}'
   end
+
   s.subspec 'Mainpage' do |ss|
       ss.source_files = 'FFAuthorKit/Classes/Mainpage/*.{h,m}'
   end
